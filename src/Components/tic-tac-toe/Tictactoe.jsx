71212c4f503ca25e9ad05440714f9ba56194f3a7
tejakoheda <kohedaraviteja@gmail.com>
2026-01-23 12:56:41 +0530

    first commit

---
+++ src/Components/tic-tac-toe/Tictactoe.jsx
@@ -0,0 +1,142 @@
+import "./Tictactoe.css";
+import circle from "../Assets/circle.png";
+import cross from "../Assets/cross.png";
+import { useRef, useState } from "react";
+
+let data = ["", "", "", "", "", "", "", "", ""];
+
+const Tictactoe = () => {
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
+
+  const toggle = (e, num) => {
+    if (lock) return 0;
+
+    if (count % 2 === 0) {
+      e.target.innerHTML = `<img src=${cross} className="icon" />`;
+      data[num] = "X";
+      setCount(++count);
+    } else {
+      e.target.innerHTML = `<img src=${circle} className="icon" />`;
+      data[num] = "O";
+      setCount(++count);
+    }
+    checkWinner();
+  };
+
+  const checkWinner = () => {
+    if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
+      won(data);
+    } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
+      won(data);
+    } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
+      won(data);
+    } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
+      won(data);
+    } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
+      won(data);
+    } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
+      won(data);
+    }
+  };
+
+  const won = (winner) => {
+    setLock(true);
+    if (winner === "X") {
+      titleRef.current.innerHTML = `Congratulations! Player \"<img src=${cross} className="icon" />\" Won`;
+    } else {
+      titleRef.current.innerHTML = `Congratulations! Player \"<img src=${circle} className="icon" />\" Won`;
+    }
+  };
+
+  const resetGame = () => {
+    data = ["", "", "", "", "", "", "", "", ""];
+    setLock(false);
+    setCount(0);
+    titleRef.current.innerHTML = "Tic Tac Toe";
+    const boxes = document.getElementsByClassName("boxes");
+    for (let box of boxes) {
+      box.innerHTML = "";
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title" ref={titleRef}>
+        Tic Tac Toe{" "}
+      </h1>
+      <div className="board">
+        <div className="row1">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+          ></div>
+        </div>
+        <div className="row2">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+          ></div>
+        </div>
+        <div className="row3">
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+          ></div>
+        </div>
+      </div>
+      <button className="reset" onClick={resetGame}>
+        Reset Game
+      </button>
+    </div>
+  );
+};
+
+export default Tictactoe;
